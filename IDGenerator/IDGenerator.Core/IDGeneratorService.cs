83b2c521c8c7929b8942e79dcebe1a5b0de44cc1
Nima Ara <NimaAra@users.noreply.github.com>
2018-10-11 20:48:30 +0100

    Avoid bound checking.

--- IDGenerator/IDGenerator.Core/IDGeneratorService.cs
+++ IDGenerator/IDGenerator.Core/IDGeneratorService.cs
@@ -186,19 +186,19 @@
 
         private static void WriteToStringMemory(Span<char> span, long id)
         {
-            span[0] = Encode_32_Chars[(int) (id >> 60) & 31];
-            span[1] = Encode_32_Chars[(int) (id >> 55) & 31];
-            span[2] = Encode_32_Chars[(int) (id >> 50) & 31];
-            span[3] = Encode_32_Chars[(int) (id >> 45) & 31];
-            span[4] = Encode_32_Chars[(int) (id >> 40) & 31];
-            span[5] = Encode_32_Chars[(int) (id >> 35) & 31];
-            span[6] = Encode_32_Chars[(int) (id >> 30) & 31];
-            span[7] = Encode_32_Chars[(int) (id >> 25) & 31];
-            span[8] = Encode_32_Chars[(int) (id >> 20) & 31];
-            span[9] = Encode_32_Chars[(int) (id >> 15) & 31];
-            span[10] = Encode_32_Chars[(int) (id >> 10) & 31];
-            span[11] = Encode_32_Chars[(int) (id >> 5) & 31];
             span[12] = Encode_32_Chars[(int) id & 31];
+            span[11] = Encode_32_Chars[(int) (id >> 5) & 31];
+            span[10] = Encode_32_Chars[(int) (id >> 10) & 31];
+            span[9] = Encode_32_Chars[(int) (id >> 15) & 31];
+            span[8] = Encode_32_Chars[(int) (id >> 20) & 31];
+            span[7] = Encode_32_Chars[(int) (id >> 25) & 31];
+            span[6] = Encode_32_Chars[(int) (id >> 30) & 31];
+            span[5] = Encode_32_Chars[(int) (id >> 35) & 31];
+            span[4] = Encode_32_Chars[(int) (id >> 40) & 31];
+            span[3] = Encode_32_Chars[(int) (id >> 45) & 31];
+            span[2] = Encode_32_Chars[(int) (id >> 50) & 31];
+            span[1] = Encode_32_Chars[(int) (id >> 55) & 31];
+            span[0] = Encode_32_Chars[(int) (id >> 60) & 31];
         }
 #endif
     }
